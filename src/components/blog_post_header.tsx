9bfcba7d6936ce76483e24a77ff3c31f628e5dc9
laboskin <artem.laboskin@acquire.io>
2024-04-19 18:13:07 +0200

    added linters

--- src/components/blog_post_header.tsx
+++ src/components/blog_post_header.tsx
@@ -1,30 +1,38 @@
-import {useConfig} from "nextra-theme-docs";
-import NextLink from "next/link"
+import { useConfig } from 'nextra-theme-docs';
 
 export default function BlogPostHeader() {
-  const { frontMatter } = useConfig()
-  const { title, tags } = frontMatter
+  const { frontMatter } = useConfig();
+  const { title, tags } = frontMatter;
   const date = new Date(frontMatter.date);
 
   return (
-    <div style={{textAlign: 'center'}}>
-      <h1 style={{fontWeight: 700, fontSize: 32}}>{title}</h1>
-      <div style={{marginBottom: 8}}>
+    <div style={{ textAlign: 'center' }}>
+      <h1 style={{ fontWeight: 700, fontSize: 32 }}>{title}</h1>
+      <div style={{ marginBottom: 8 }}>
         <time dateTime={date.toISOString()}>
-          {date.toLocaleDateString("en", {
-            month: "long",
-            day: "numeric",
-            year: "numeric",
+          {date.toLocaleDateString('en', {
+            month: 'long',
+            day: 'numeric',
+            year: 'numeric',
           })}
         </time>
       </div>
-      <div style={{display: "flex", justifyContent: "center", alignItems: 'center', gap: 8}}>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: 8 }}>
         {tags.map((tag: string) => (
-          <span key={tag} style={{border: '1px solid currentColor', borderRadius: 4, padding: '2px 6px', fontSize: 14, fontWeight: 700}}>
-            {tag.replaceAll("-", " ")}
+          <span
+            key={tag}
+            style={{
+              border: '1px solid currentColor',
+              borderRadius: 4,
+              padding: '2px 6px',
+              fontSize: 14,
+              fontWeight: 700,
+            }}
+          >
+            {tag.replaceAll('-', ' ')}
           </span>
         ))}
       </div>
     </div>
-  )
+  );
 }
